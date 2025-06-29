98a6ea3742e3e8b035f6bbfb5975acfa2274e47d
lpender3672 <58957722+lpender3672@users.noreply.github.com>
2025-06-29 20:03:42 +0100

    updated 22 state derivation 12+ years in  matlab version. still took 2hrs

--- derivations/OptimiseAlgebra.m
+++ derivations/OptimiseAlgebra.m
@@ -1,29 +1,42 @@
 function [SymExpOut,SubExpArray] = OptimiseAlgebra(SymExpIn,SubExpName)
-
 % Loop through symbolic expression, identifying repeated expressions and
 % bringing them out as shared expression or sub expressions
 % do this until no further repeated expressions found
 % This can significantly reduce computations
-
-syms SubExpIn SubExpArray ;
-
+syms SubExpIn SubExpArray;
 SubExpArray(1,1) = 'invalid';
 index = 0;
 f_complete = 0;
 while f_complete==0
     index = index + 1;
-    SubExpIn = [SubExpName,'(',num2str(index),')'];
-    SubExpInStore{index} = SubExpIn;
+    
+    % Create the symbolic variable name
+    varName = [SubExpName,'_',num2str(index),'_'];  % Use underscore instead of parentheses
+    
+    % Create the symbolic variable dynamically
+    eval(['syms ', varName, ' real']);
+    
+    % Store the variable name for comparison
+    SubExpInStore{index} = varName;
+    
+    % Get the symbolic variable
+    SubExpIn = eval(varName);
+    
+    % Find sub-expressions
     [SymExpOut,SubExpOut]=subexpr(SymExpIn,SubExpIn);
-        for k = 1:index
-            if SubExpOut == SubExpInStore{k}
-                f_complete = 1;
-            end
-        end
-    if f_complete || index > 100
-        SymExpOut = SymExpIn;
-    else
+    
+    % Check if we found any new sub-expressions
+    f_complete = 1;  % Assume we're done
+    if ~isequal(SymExpOut, SymExpIn)  % If something changed
+        f_complete = 0;  % We're not done yet
         SubExpArray(index,1) = SubExpOut;
         SymExpIn = SymExpOut;
     end
+    
+    % Safety check to prevent infinite loop
+    if index > 100
+        f_complete = 1;
+        SymExpOut = SymExpIn;
+    end
+end
 end
